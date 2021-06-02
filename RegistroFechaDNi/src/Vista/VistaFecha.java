8705eea1f218b6fb8da01983967588247001572e
ChristianBuestan <84852007+ChristianBuestan@users.noreply.github.com>
2021-06-02 14:06:52 -0500

    Add files via upload

---
+++ RegistroFechaDNi/src/Vista/VistaFecha.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vista;
+
+import Controlador.ControladorFecha;
+import java.util.Scanner;
+import modelo.Fecha;
+
+/**
+ *
+ * @author Cristian
+ */
+public class VistaFecha {
+    private ControladorFecha controladorFecha;
+    public Scanner teclado;
+    public VistaFecha(ControladorFecha controladorFecha){
+        this.controladorFecha=controladorFecha;
+        teclado=new Scanner(System.in);
+    }
+    public void menu(){
+        int opcion=0;
+        do{
+            System.out.println(" 1.crear \n 2.Actualizar \n 3.Buscar \n 4.eliminar \n 5.listar \n 6.salir");
+            opcion=teclado.nextInt();
+            switch(opcion){
+                case 1: this.crear();break;
+                case 2:this.Actualizar();break;
+                case 3:this.buscar();break;
+                case 4:this.eliminar();break;
+                case 5:this.listar();break;
+            }
+        }while(opcion<6);
+    }
+    public void crear(){
+        System.out.println("Ingrese \n dia");
+        int dia=teclado.nextInt();
+        System.out.println("mes en numero");
+        int mes=teclado.nextInt();
+        System.out.println("anio en numero");
+        int anio=teclado.nextInt();
+        System.out.println("creado "+ controladorFecha.crear(dia, mes, anio));
+    }
+    public void Actualizar(){
+        System.out.println("Ingrese \n dia");
+        int dia=teclado.nextInt();
+        System.out.println("mes en numero");
+        int mes=teclado.nextInt();
+        System.out.println("anio en numero");
+        int anio=teclado.nextInt();
+        System.out.println("creado "+ controladorFecha.actualizar(dia, mes, anio));
+    }
+    public Fecha buscar(){
+        System.out.println("Ingrese \n dia");
+        int dia=teclado.nextInt();
+        System.out.println(" mes en numero");
+        int mes=teclado.nextInt();
+        System.out.println("encontrado "+controladorFecha.buscar(dia, mes));
+        return controladorFecha.buscar(dia, mes);
+    }
+    public void eliminar(){
+        this.buscar();
+        System.out.println(controladorFecha.eliminar(controladorFecha.getSeleccionado().getDia(),controladorFecha.getSeleccionado().getMes()));
+    }
+    public void listar(){
+        for (Fecha fecha : controladorFecha.getListaFecha()) {
+            System.out.println(fecha);
+        }
+        
+    }
+    
+}
