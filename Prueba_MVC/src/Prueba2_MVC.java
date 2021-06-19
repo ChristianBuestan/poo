293578c90d8e9d242b0b72f5571aa2deed592f5f
ChristianBuestan <84852007+ChristianBuestan@users.noreply.github.com>
2021-06-19 18:25:28 -0500

    Primer git
    20/06/2021

---
+++ Prueba_MVC/src/Prueba2_MVC.java
@@ -0,0 +1,24 @@
+
+import Vista.VistaGeneral;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Cristian
+ */
+public class Prueba2_MVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VistaGeneral vista=new VistaGeneral();
+        vista.menu();
+    }
+    
+}
